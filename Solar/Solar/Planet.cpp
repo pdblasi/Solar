b3041b790208d7006e72b3483ddbe1ac7d57e328
pdblasi <pdblasi@gmail.com>
2014-11-23 16:02:28 -0700

    MOre stuff!

--- Solar/Solar/Planet.cpp
+++ Solar/Solar/Planet.cpp
@@ -144,6 +144,7 @@ void Planet::Draw(ProgramState state)
 
 		if (state.Wireframe)
 		{
+			glEnable(GL_COLOR_MATERIAL);
 			//Enable Wireframe
 			glColor4fv(_color);
 			gluQuadricDrawStyle(_planet, GLU_LINE);
@@ -151,7 +152,7 @@ void Planet::Draw(ProgramState state)
 		}
 		else
 		{
-			glColor4fv(_color);
+			glDisable(GL_COLOR_MATERIAL);
 			glMaterialfv(GL_FRONT, GL_DIFFUSE, _color);
 			glMaterialfv(GL_FRONT, GL_AMBIENT, _color);
 			gluQuadricDrawStyle(_planet, GLU_FILL);
